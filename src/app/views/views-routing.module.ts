0b679d23926fcd17efa1591e586e732c8e56d8e3
LindLz <zogajlind@gmail.com>
2024-02-25 22:14:58 +0100

    created project structure , and CRUD operations

---
+++ src/app/views/views-routing.module.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from "@angular/router";
+import { ViewsComponent } from "./views.component";
+
+
+
+
+const routes: Routes = [
+    { path: '', component: ViewsComponent, children: [
+     { path: '', loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule) },
+     { path: '', loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesModule) },
+    ]
+    }
+  
+];
+export const ViewsRoutingModule = RouterModule.forChild(routes);
